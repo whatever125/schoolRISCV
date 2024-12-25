a1c90f1b88874a9150e0b62637877ef4eefdc22f
Dmitriy Kolmakov <kolmakov125@gmail.com>
2024-12-25 17:15:25 +0300

    fix test

--- testbench/testbench.v
+++ testbench/testbench.v
@@ -115,8 +115,8 @@ module sm_testbench;
 
     always @ (posedge clk)
     begin
-        $write ("%5d  pc = %2h instr = %h a0 = %1d", 
-                  cycle, sm_top.sm_cpu.pc, sm_top.sm_cpu.instr, sm_top.sm_cpu.rf.rf[10]);
+        $write ("%5d  pc = %2h instr = %h a0 = %1d a1 = %1d a2 = %1d a3 = %1d\n      ", 
+                  cycle, sm_top.sm_cpu.pc, sm_top.sm_cpu.instr, sm_top.sm_cpu.rf.rf[10], sm_top.sm_cpu.rf.rf[11], sm_top.sm_cpu.rf.rf[12], sm_top.sm_cpu.rf.rf[13]);
 
         disasmInstr();
 
